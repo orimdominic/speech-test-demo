3f34f32a7121282670fecef8a1a02cc9c2f57b6e
Orim Dominic <orimdominic@gmail.com>
2022-11-09 08:36:13 +0100

    fix: re user mic perms before recording

--- script.js
+++ script.js
@@ -247,6 +247,13 @@ function showButtons(style) {
 }
 
 function startTest() {
+  navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(function(stream) {
+        console.log('You let me use your mic!')
+      })
+      .catch(function(err) {
+        console.log('No mic for you!')
+      });
   if (currentPhraseIndex >= phrases.length) {
     return;
   }
